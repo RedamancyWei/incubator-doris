fb7f8807ca31b40979204e676cdad6ccf12cb4f9
ElvinWei <zhengte.wei@outlook.com>
2022-07-08 00:12:40 +0800

    for dev

--- fe/fe-core/src/main/java/org/apache/doris/statistics/SQLStatisticsTask.java
+++ fe/fe-core/src/main/java/org/apache/doris/statistics/SQLStatisticsTask.java
@@ -17,11 +17,17 @@
 
 package org.apache.doris.statistics;
 
+import org.apache.doris.catalog.Catalog;
+import org.apache.doris.catalog.Database;
+import org.apache.doris.catalog.Table;
+import org.apache.doris.common.DdlException;
 import org.apache.doris.statistics.StatisticsTaskResult.TaskResult;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.IntStream;
 
 /*
@@ -29,7 +35,7 @@ A statistics task that collects statistics by executing query.
 The results of the query will be returned as @StatisticsTaskResult.
  */
 public class SQLStatisticsTask extends StatisticsTask {
-    private String sql;
+    private String query;
 
     public SQLStatisticsTask(long jobId, List<StatisticsDesc> statsDescs) {
         super(jobId, statsDescs);
@@ -56,10 +62,51 @@ public class SQLStatisticsTask extends StatisticsTask {
         return new StatisticsTaskResult(taskResults);
     }
 
-    protected void constructQuery(StatisticsDesc statsDesc) {
+    protected String constructQuery(StatisticsDesc statsDesc) throws DdlException {
+        Map<String, String> params = getQueryParams(statsDesc);
+        List<StatsType> statsTypes = statsDesc.getStatsTypes();
+        for (StatsType statsType : statsTypes) {
+            switch (statsType) {
+                case ROW_COUNT:
+                    return getRowContQuery(params);
+                case NUM_NULLS:
+                    return getNumNullsQuery(params);
+                case MAX_SIZE:
+                case AVG_SIZE:
+                    return getMaxMinSizeQuery(params);
+                case NDV:
+                case MAX_VALUE:
+                case MIN_VALUE:
+                    return getNdvMaxMinValueQuery(params);
+                case DATA_SIZE:
+                default:
+                    return null;
+            }
+        }
+        return null;
         // TODO
         // step1: construct FROM by @granularityDesc
         // step2: construct SELECT LIST by @statsTypeList
+    }
+
+    private String getNdvMaxMinValueQuery(Map<String, String> params) {
+        return null;
+    }
+
+    private String buildRowContQuery(Map<String, String> params) {
+        return null;
+    }
+
+    private String getMaxMinSizeQuery(Map<String, String> params) {
+        return null;
+    }
+
+    private String getNumNullsQuery(Map<String, String> params) {
+        return null;
+    }
+
+    private String getRowContQuery(Map<String, String> params) {
+        return null;
     }
 
     protected TaskResult executeQuery(StatisticsDesc statsDesc) {
@@ -84,5 +131,18 @@ public class SQLStatisticsTask extends StatisticsTask {
     protected StatisticsTaskResult constructTaskResult(List<String> queryResultList) {
         // TODO
         return null;
+    }
+
+    private Map<String, String> getQueryParams(StatisticsDesc statsDesc) throws DdlException {
+        StatsCategory category = statsDesc.getCategory();
+        Database db = Catalog.getCurrentCatalog().getDbOrDdlException(category.getDbId());
+        Table table =  db.getTableOrDdlException(category.getTableId());
+
+        Map<String, String> params = Maps.newHashMap();
+        params.put("database", db.getFullName());
+        params.put("table", table.getName());
+        params.put("partition", category.getPartitionName());
+        params.put("column", category.getColumnName());
+        return params;
     }
 }
